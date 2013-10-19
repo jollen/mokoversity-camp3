841b469a22ce4197872493f305c2f5537eff4588
Jollen Chen <jollen@moko365.com>
2013-10-19 16:28:57 +0800

    Fix 9: add background image using CSS

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ var gameModule = (function() {
 	function start() {
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
-	    var ballX = Math.floor(Math.random() * 300); // 0..300
-	    var ballY = Math.floor(Math.random() * 500);
-	    var ballR = Math.floor(Math.random() * 100);
+	    var ballX = Math.floor(Math.random() * 600); // 0..300
+	    var ballY = Math.floor(Math.random() * 450);
+	    var ballR = Math.floor(Math.random() * 80);
 
-        canvas.width = 480;
-        canvas.height = 320;
+        canvas.width = 640;
+        canvas.height = 480;
 
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = '#ff0000';
         ctx.beginPath();
         ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
         ctx.fill();
