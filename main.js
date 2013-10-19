c650d277cfe0cdf13622bf06dd7352340ce3eabd
Jollen Chen <jollen@moko365.com>
2013-10-19 15:02:14 +0800

    Fix #8

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var gameModule = (function() {
+
+	var timeoutVar,
+		counter = 0;
+
 	function start() {
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
@@ -13,6 +17,15 @@ var gameModule = (function() {
         ctx.beginPath();
         ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
         ctx.fill();
+
+        if (counter >= 10) {
+
+        } else {
+	        timeoutVar = setTimeout(start, 1000);
+	        counter = counter + 1;
+	        
+	        console.log("Counter: " + counter);
+	    } 
 	}
 
 	return {
